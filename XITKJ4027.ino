c3acbfee63280be31092bbd46cb358a0a6bb8de9
restumegan <restumega21@gmail.com>
2016-10-03 13:14:52 +0700

    Add files via upload

---
+++ XITKJ4027.ino
@@ -0,0 +1,21 @@
+/* 
+  Blink
+  Menyalakan LED pada pin 4 selama 4 detik dan Mematikanya selama 4 detik.
+
+   Ini contoh kode Programnya.
+ */
+
+ int led = 4 ;//Output pada Digital Pin adalah sebuah LED yang terkoneksi/terhubung dengan Digital pin 4.
+  
+void setup() 
+{
+  pinMode(led, OUTPUT);// Mengatur Digital Pin sebagai Output.
+
+}
+
+void loop() {
+  digitalWrite(led, HIGH);          // Mengatur LED agar Menyala.
+  delay(2500);                      // Tunggu sampai 4 detik.
+  digitalWrite(led ,LOW);           // Mengatur LED agar Mati.
+  delay(2500);                      // Tunggu sampai 4 detik. 
+}
